7cde12356678007d1834edb62031fecf6354fa2e
JBT_real <tobjearllan@gmail.com>
2026-06-09 12:07:07 +0800

    practice website

---
+++ index.js
@@ -0,0 +1,29 @@
+//day 1: 1:53:42;
+//day 2: 2:23:08;
+//day 3: 3:58:42;
+//day 4; 5:17:42;
+//day 5: 6:31:46;
+//day 6: --N/A--;
+//day 7: 6:40:08;
+//day 8: 7:34:13;
+const get_answer= document.getElementById("get_answer");
+const text = document.getElementById("text");
+const text2 = document.getElementById("text2");
+const submitbtn = document.getElementById("submit");
+
+const fruits = ["apple", "pinespple", "banana", "mango", "coconut"];
+
+const numbers = [1, 10, 3, 2, 7, 8,9 ,5 ,4, 6 ];
+
+fruits.sort();
+
+console.log(fruits);
+
+const date = new Date();
+const yer = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDay();
+
+console.log(yer);
+console.log(month);
+console.log(day);
