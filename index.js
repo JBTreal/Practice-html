e3d2606dbd11db2fd6a42d813a9e57659c95f511
JBT_real <tobjearllan@gmail.com>
2026-06-09 13:36:23 +0800

    asynchronous and synchronous

--- index.js
+++ index.js
@@ -15,11 +15,15 @@ const submitbtn = document.getElementById("submit");
 
 import {PI, getCircumference, getArea, getVolume} from './mathUtil.js';
 
-console.log(PI);
-const circumference = getCircumference(10);
-const area = getArea(10);
-const volume = getVolume(10);
+function func1(callback){
+setTimeout(() => {console.log("Task 1"); callback()}, 3000)
+}
 
-console.log(volume.toFixed(2));
-console.log(area.toFixed(2));
-console.log(circumference.toFixed(2));
+function func2(){
+console.log("Task 2");
+console.log("Task 3");
+console.log("Task 4");
+console.log("Task 5");
+}
+
+func1(func2);
